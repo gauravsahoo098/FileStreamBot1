11d221778940f1ad567048febea35757af5212ae
gauravsahoo098 <159449508+gauravsahoo098@users.noreply.github.com>
2024-03-01 00:08:04 +0530

    Update config.py

--- FileStream/config.py
+++ FileStream/config.py
@@ -6,11 +6,11 @@ load_dotenv()
 class Telegram:
     API_ID = int(env.get("API_ID", '20108610'))
     API_HASH = str(env.get("API_HASH", '262418f5ebe0a599821a5802a90ee1a5'))
-    BOT_TOKEN = str(env.get("BOT_TOKEN", '6590978998:AAFxHZcqaRmDUeZ64mk9pCYy-n08bYVyAtI'))
+    BOT_TOKEN = str(env.get("BOT_TOKEN", '7196853520:AAH8qexWPYMRdBmdG0YifjnMyMiC8T_FQcE'))
     OWNER_ID = int(env.get('OWNER_ID', '2014953230'))
-    WORKERS = int(env.get("WORKERS", "6"))  # 6 workers = 6 commands at once
+    WORKERS = int(env.get("WORKERS", "30"))  # 6 workers = 6 commands at once
     DATABASE_URL = str(env.get('DATABASE_URL', 'mongodb+srv://epiksam14:epiksam14@cluster0.tbudcc3.mongodb.net/?retryWrites=true&w=majority'))
-    UPDATES_CHANNEL = str(env.get('UPDATES_CHANNEL', "TADxBotz"))
+    UPDATES_CHANNEL = str(env.get('UPDATES_CHANNEL', "swegos"))
     SESSION_NAME = str(env.get('SESSION_NAME', 'FileStream'))
     FORCE_SUB_ID = env.get('FORCE_SUB_ID', None)
     FORCE_SUB = env.get('FORCE_UPDATES_CHANNEL', False)
@@ -20,8 +20,8 @@ class Telegram:
     START_PIC = env.get('START_PIC', "https://graph.org/file/290af25276fa34fa8f0aa.jpg")
     VERIFY_PIC = env.get('VERIFY_PIC', "https://graph.org/file/736e21cc0efa4d8c2a0e4.jpg")
     MULTI_CLIENT = False
-    FLOG_CHANNEL = int(env.get("FLOG_CHANNEL", "-1002040564082"))   # Logs channel for file logs
-    ULOG_CHANNEL = int(env.get("ULOG_CHANNEL", "-1002040564082"))   # Logs channel for user logs
+    FLOG_CHANNEL = int(env.get("FLOG_CHANNEL", "-1002034870962"))   # Logs channel for file logs
+    ULOG_CHANNEL = int(env.get("ULOG_CHANNEL", "-1002034870962"))   # Logs channel for user logs
     MODE = env.get("MODE", "primary")
     SECONDARY = True if MODE.lower() == "secondary" else False
     AUTH_USERS = list(set(int(x) for x in str(env.get("AUTH_USERS", "")).split()))
@@ -32,7 +32,7 @@ class Server:
     PING_INTERVAL = int(env.get("PING_INTERVAL", "1200"))
     HAS_SSL = str(env.get("HAS_SSL", "0").lower()) in ("1", "true", "t", "yes", "y")
     NO_PORT = str(env.get("NO_PORT", "1").lower()) in ("1", "true", "t", "yes", "y")
-    FQDN = str(env.get("FQDN", "103.248.61.166:9090"))
+    FQDN = str(env.get("FQDN", "103.212.120.43:9090"))
     URL = "http{}://{}{}/".format(
         "s" if HAS_SSL else "", FQDN, "" if NO_PORT else ":" + str(PORT)
     )
