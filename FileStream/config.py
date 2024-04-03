2cb8831aec0a53d592f24489978592f53c831f78
gauravsahoo098 <159449508+gauravsahoo098@users.noreply.github.com>
2024-04-04 05:18:42 +0530

    Update config.py

--- FileStream/config.py
+++ FileStream/config.py
@@ -30,11 +30,11 @@ class Server:
     PORT = int(env.get("PORT", "80"))
     BIND_ADDRESS = str(env.get("BIND_ADDRESS", "0.0.0.0"))
     PING_INTERVAL = int(env.get("PING_INTERVAL", "1200"))
-    HAS_SSL = str(env.get("HAS_SSL", "0").lower()) in ("1", "true", "t", "yes", "y")
+    HAS_SSL = str(env.get("HAS_SSL", "1").lower()) in ("1", "true", "t", "yes", "y")
     NO_PORT = str(env.get("NO_PORT", "1").lower()) in ("1", "true", "t", "yes", "y")
-    FQDN = str(env.get("FQDN", "154.41.252.55:80"))
+    FQDN = str(env.get("FQDN", "uclaadhar.cloud"))
     URL = "http{}://{}{}/".format(
-        "s" if HAS_SSL else "", FQDN, "154.41.252.55" if NO_PORT else ":" + str(PORT)
+        "s" if HAS_SSL else "1", FQDN, "uclaadhar.cloud" if NO_PORT else ":" + str(PORT)
     )
 
 
